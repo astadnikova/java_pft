3f67cf264c0585d7b9dc83c6f9e0a9ecbd3f58a8
astadnikova <32067882+astadnikova@users.noreply.github.com>
2017-09-27 11:19:00 +0300

    Задание №2

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -3,21 +3,21 @@ package ru.stqa.pft.sandbox;
 public class MyFirstProgram {
 
     public static void main(String[] args) {
-        hello ("world");
-        hello ("user");
-        hello ("Alexei");
 
+        Point p1;
+        Point p2;
+        p1=new Point(2,3);
+        p2=new Point (3, 4);
+        System.out.println(distance(p1,p2));
+        System.out.println(p1.distance(p1,p2));
 
-        Square s = new Square(5);
-        s.l = 5;
-        System.out.println("Площадь квадрата со стороной " + s.l  + " = " + s.area());
+    }
 
-        Rectangle r = new Rectangle(4, 6);
-        System.out.println("Площадь прямоугольника со сторонами " + r.a +  " и " + r.b +  " = " + r.area());
+    public static double distance(Point p1, Point p2){
+
+      return  Math.sqrt (Math.pow(p2.x-p1.x, 2)+ Math.pow(p1.y-p2.y, 2));
     }
 
 
-    public static void hello (String somebody) {
-        System.out.println("Hello, " + somebody + "!");
-    }
+
 }
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -1,4 +1,16 @@
 package ru.stqa.pft.sandbox;
 
 public class Point {
+    double x, y;
+
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distance(Point p1, Point p2){
+
+        return  Math.sqrt (Math.pow(p2.x-p1.x, 2)+ Math.pow(p1.y-p2.y, 2));
+    }
 }
